ee25238ddc1c4ba0e0dc8941a990f6cb100b962a
chitta1040 <52632041+chitta1040@users.noreply.github.com>
2021-02-10 01:32:36 +0530

    Update ip6.sh

--- scripts/ip6.sh
+++ scripts/ip6.sh
@@ -18,7 +18,11 @@ gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
          IP66=$(gen64 $IP6)
          echo "proxy -6 -n -a -p$port -i $IP4 -e $IP66"
-	 netsh interface ipv6 add address 5 $IP66	
+	 netsh interface ipv6 add address 5 $IP66
+	
+	 cat >proxy.txt <<EOF
+         $(awk -F "/" '{print "proxy -6 -n -a -p" $port " -i" $IP4 " -e"$IP66 "\n" }' ${WORKDATA})
+         EOF
     done
 }
 
