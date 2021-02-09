140f244264183512a12f530e8d65abde7ec27086
chitta1040 <52632041+chitta1040@users.noreply.github.com>
2021-02-10 03:50:46 +0530

    Update required.sh

--- scripts/required.sh
+++ scripts/required.sh
@@ -3,3 +3,4 @@
 pip install selenium
 pip install requests
 pip install pyautogui
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters" /v DisabledComponents /t REG_DWORD /d 0x00 /f
