db887d8272b5bcbd52ade890d937973be838d990
chitta1040 <52632041+chitta1040@users.noreply.github.com>
2021-05-04 01:22:17 +0530

    Update folders.sh

--- scripts/folders.sh
+++ scripts/folders.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # A bash script to create folders with subfolders all in one go.
-WORKDIR="C:\Users\Administrator\Desktop\"
+WORKDIR= "C:\Users\Administrator\Desktop"
 mkdir $WORKDIR && cd $_
 
 $ mkdir -p {YT}/{cookies,log,driver,main,views}
