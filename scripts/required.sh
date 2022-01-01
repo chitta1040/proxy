053e9f8cb7211b815c0d189892b50c3de89c631b
chitta1040 <52632041+chitta1040@users.noreply.github.com>
2022-01-01 22:35:19 +0530

    Update required.sh

--- scripts/required.sh
+++ scripts/required.sh
@@ -2,5 +2,6 @@
 
 pip install selenium
 pip install requests
-pip install pyautogui
+pip install selenium-wire
+pip install Faker
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters" /v DisabledComponents /t REG_DWORD /d 0x00 /f
